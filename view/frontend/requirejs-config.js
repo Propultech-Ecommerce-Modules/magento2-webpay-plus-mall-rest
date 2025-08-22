9be2617e67db1ff9b608349f6f9d3a9411efeadf
Jhonny Negrin <jhonny.negrin@propul.tech>
2025-08-22 19:45:21 -0400

    feat: improve mixin config structure and expose payment config to checkout
    - Refactor `requirejs-config.js` to streamline mixin declarations.
    - Add configuration provider to expose payment settings in checkout UI.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,11 +1,9 @@
 var config = {
-    map: {
-        '*': {
-            'Magento_Checkout/js/model/place-order': 'Propultech_WebpayPlusMallRest/js/model/place-order-mixin'
-        }
-    },
     config: {
         mixins: {
+            'Magento_Checkout/js/model/place-order': {
+                'Propultech_WebpayPlusMallRest/js/model/place-order-mixin': true
+            },
             'Magento_Checkout/js/action/set-payment-information': {
                 'Propultech_WebpayPlusMallRest/js/action/set-payment-information-mixin': true
             }
