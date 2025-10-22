ce403aaf32712a58c5605e1384136980e45d7799
Jhonny Negrin <jhonny.negrin@propul.tech>
2025-10-22 15:49:16 -0300

    feat: enhance error handling for Webpay Plus Mall payment actions
    - Add modal alerts for payment information and transaction errors.
    - Display specific error messages from server responses when available.
    - Update transaction creation to handle failures gracefully.
    - Improve user feedback by replacing inline messages with modal alerts in multiple scenarios.

--- view/frontend/web/js/action/set-payment-information-mixin.js
+++ view/frontend/web/js/action/set-payment-information-mixin.js
@@ -1,17 +1,34 @@
 define([
     'jquery',
     'mage/utils/wrapper',
-    'Magento_Checkout/js/model/quote'
-], function ($, wrapper, quote) {
+    'Magento_Checkout/js/model/quote',
+    'Magento_Ui/js/modal/alert'
+], function ($, wrapper, quote, alert) {
     'use strict';
 
     return function (setPaymentInformationAction) {
         return wrapper.wrap(setPaymentInformationAction, function (originalAction, messageContainer, paymentData) {
-            if (paymentData.method === 'propultech_webpayplusmall') {
-                // For Webpay Plus Mall, we don't need to modify the behavior
-                // This mixin is here in case we need to add custom logic in the future
+            var result = originalAction(messageContainer, paymentData);
+
+            if (paymentData && paymentData.method === 'propultech_webpayplusmall') {
+                // Intercept failures to show modal alert instead of inline messages
+                if (result && typeof result.fail === 'function') {
+                    result.fail(function (xhr) {
+                        var msg = 'Error al guardar la información de pago.';
+                        try {
+                            var json = xhr && xhr.responseJSON;
+                            if (json && json.message) {
+                                msg = json.message;
+                            }
+                        } catch (e) {}
+                        alert({
+                            content: msg
+                        });
+                    });
+                }
             }
-            return originalAction(messageContainer, paymentData);
+
+            return result;
         });
     };
 });
--- view/frontend/web/js/model/place-order-mixin.js
+++ view/frontend/web/js/model/place-order-mixin.js
@@ -6,8 +6,8 @@ define([
     'Magento_Checkout/js/model/url-builder',
     'mage/url',
     'Magento_Checkout/js/model/error-processor',
-    'Magento_Ui/js/model/messageList'
-], function ($, wrapper, quote, customer, urlBuilder, url, errorProcessor, messageList) {
+    'Magento_Ui/js/modal/alert'
+], function ($, wrapper, quote, customer, urlBuilder, url, errorProcessor, alert) {
     'use strict';
 
     return function (placeOrderService) {
@@ -17,7 +17,7 @@ define([
 
                 // Call the original action to place the order
                 originalAction(serviceUrl, payload, messageContainer).done(function (response) {
-                    // Redirect to Webpay Plus Mall
+                    // Create Webpay Plus Mall transaction
                     $.ajax({
                         url: url.build('propultech_webpayplusmall/transaction/create'),
                         type: 'post',
@@ -25,36 +25,44 @@ define([
                         data: {
                             'order_id': response
                         }
-                    }).done(function (response) {
-                        if (response.url && response.token_ws) {
-                            // Create form to redirect to Webpay
+                    }).done(function (resp) {
+                        if (resp && resp.url && resp.token_ws) {
+                            // Create form and submit to redirect to Webpay
                             var form = $('<form>', {
-                                'action': response.url,
+                                'action': resp.url,
                                 'method': 'post'
                             });
                             form.append($('<input>', {
                                 'name': 'token_ws',
-                                'value': response.token_ws,
+                                'value': resp.token_ws,
                                 'type': 'hidden'
                             }));
                             form.appendTo('body').submit();
-                        } else if (response.error) {
-                            messageList.addErrorMessage({
-                                message: response.error
-                            });
-                            deferred.reject();
+                            // Do not resolve deferred to prevent default success redirect
                         } else {
-                            messageList.addErrorMessage({
-                                message: 'Error al procesar el pago con Webpay Plus Mall'
+                            var msg = (resp && resp.error) ? resp.error : 'Error al procesar el pago con Webpay Plus Mall';
+                            alert({
+                                content: msg
                             });
+                            // Explicitly reject to block success redirect
                             deferred.reject();
                         }
-                    }).fail(function (response) {
-                        errorProcessor.process(response, messageContainer);
+                    }).fail(function (xhr) {
+                        // Show generic error via modal and reject
+                        var msg = 'Error al crear la transacción con Webpay Plus Mall.';
+                        try {
+                            var json = xhr && xhr.responseJSON;
+                            if (json && json.message) {
+                                msg = json.message;
+                            }
+                        } catch (e) {}
+                        alert({
+                            content: msg
+                        });
                         deferred.reject();
                     });
-                }).fail(function (response) {
-                    errorProcessor.process(response, messageContainer);
+                }).fail(function (xhr) {
+                    errorProcessor.process(xhr, messageContainer);
                     deferred.reject();
                 });
 
